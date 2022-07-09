2522ed341414472c64416ead9db10c9d1d8133ab
星梦喵吖 <starsdream00@icloud.com>
2022-07-09 18:16:40 +0800

    优化

--- MotdBEAPI/Main.cs
+++ MotdBEAPI/Main.cs
@@ -8,15 +8,11 @@ namespace MotdBEAPI {
         public string Introduction => "MotdBEAPI";
         public Dictionary<string, string> MetaData => new();
         public Version Version => new(1, 0, 0);
-        private static readonly string str = "01000000000000000000FFFF00FEFEFEFEFDFDFDFD123456780000000000000000";
-        private static readonly byte[] data = new byte[str.Length / 2];
+        private static readonly byte[] data = new byte[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 0, 254, 254, 254, 254, 253, 253, 253, 253, 18, 52, 86, 120, 0, 0, 0, 0, 0, 0, 0, 0 };
         public void OnInitialize() {
-            for (int i = 0; i < str.Length; i += 2) {
-                data[i / 2] = Convert.ToByte(str.Substring(i, 2), 16);
-            }
-            _ = LLNET.RemoteCall.DynamicRemoteCallAPI.ExportAs("MotdBE", "MotdBE", (string ip, ushort port) => {
+            _ = LLNET.RemoteCall.RemoteCallAPI.ExportAs("MotdBE", "GetMotd", (string ip, ushort port) => {
                 Socket socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                byte[] back = new byte[255];
+                byte[] back = new byte[256];
                 socket.Connect(ip, port);
                 _ = socket.Send(data);
                 _ = socket.Receive(back);

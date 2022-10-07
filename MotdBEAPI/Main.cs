02ab869850a7a5ba6debf96d634bd96cfc4ec3df
星梦StarsDream <100036558+StarsDream00@users.noreply.github.com>
2022-10-07 17:46:31 +0800

    Update Main.cs

--- MotdBEAPI/Main.cs
+++ MotdBEAPI/Main.cs
@@ -2,15 +2,15 @@
 using System.Net.Sockets;
 using System.Text;
 
-namespace MOTDBEAPI {
-    [PluginMain("MOTDBEAPI")]
+namespace MOTDAPI {
+    [PluginMain("MOTDAPI")]
     public class Main : IPluginInitializer {
-        public string Introduction => "MOTDBEAPI";
+        public string Introduction => "MOTDAPI";
         public Dictionary<string, string> MetaData => new();
         public Version Version => new(1, 0, 0);
         private static readonly byte[] data = new byte[] { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 0, 254, 254, 254, 254, 253, 253, 253, 253, 18, 52, 86, 120, 0, 0, 0, 0, 0, 0, 0, 0 };
         public void OnInitialize() {
-            LLNET.RemoteCall.RemoteCallAPI.ExportAs("MOTD", "GetBEMOTD", (string ip, ushort port) => {
+            LLNET.RemoteCall.RemoteCallAPI.ExportAs("MOTD", "GetFromBE", (string ip, ushort port) => {
                 Socket socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
                 byte[] back = new byte[256];
                 try {

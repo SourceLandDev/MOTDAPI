eac17189cc11fd9f95b4025026404bc4ca9b2eaa
星梦 <starsdream00@icloud.com>
2023-08-16 15:33:19 +0800

    修复

--- src/Main.cs
+++ src/Main.cs
@@ -1,9 +1,9 @@
-using LiteLoader.Logger;
-using LiteLoader.NET;
-using LiteLoader.RemoteCall;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using LiteLoader.NET;
+using LiteLoader.RemoteCall;
+using LiteLoader.Schedule;
 
 namespace MOTDAPI;
 [PluginMain("MOTDAPI")]
@@ -12,63 +12,113 @@ public class Main : IPluginInitializer
     public string Introduction => "MOTDAPI";
     public Dictionary<string, string> MetaData => new();
     public Version Version => new();
-    private static readonly byte[] BEData = { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 0, 254, 254, 254, 254, 253, 253, 253, 253, 18, 52, 86, 120, 0, 0, 0, 0, 0, 0, 0, 0 };
-    private static readonly byte[] JEData = { 6, 0, 0, 0, 0x63, 0xdd, 1, 1, 0 };
+    private static readonly byte[] s_bedrockEditionUnconnectedPong = { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 0, 254, 254, 254, 254, 253, 253, 253, 253, 18, 52, 86, 120, 0, 0, 0, 0, 0, 0, 0, 0 };
+    private static readonly byte[] s_javaEditionStatusRequest = { 6, 0, 0, 0, 0x63, 0xdd, 1, 1, 0 };
     public void OnInitialize()
     {
-        Logger logger = new("MOTDAPI");
-        RemoteCallAPI.ExportAs("MOTDAPI", "GetFromBE", (string addr, ushort port) =>
+        RemoteCallAPI.ExportFunc("MOTDAPI", "GetFromBE", (List<Valuetype> args) => GetFromBedrockEdition(args[0], args[1], args[2]));
+        RemoteCallAPI.ExportFunc("MOTDAPI", "GetFromBEAsync", (List<Valuetype> args) =>
         {
-            byte[] back = new byte[1024]; // 防止返回内容过多导致撑爆字节数组
-            int length = 0;
-            try
+            Task<string> task = Task.Run(() => GetFromBedrockEdition(args[0], args[1], args[4]));
+            task.ContinueWith((task) =>
             {
-                IPAddress ip = GetIP(addr);
-                Socket socket = new(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
-                socket.Connect(ip, port);
-                socket.Send(BEData);
-                length = socket.Receive(back);
-            } catch (SocketException ex)
-            {
-                logger.Debug.WriteLine(ex);
-            }
-            return length > 35 ?
-                   Encoding.UTF8.GetString(back, 35, length - 35) : // 基岩版服务器返回数据包头部有35个byte无效
-                   Encoding.UTF8.GetString(back);
+                if (!RemoteCallAPI.HasFunc(args[2], args[3]))
+                {
+                    return;
+                }
+                ScheduleAPI.NextTick(() =>
+                    RemoteCallAPI.ImportFunc(args[2], args[3])(new() { task.Id, task.Result }));
+            });
+            return task.Id;
         });
-        RemoteCallAPI.ExportAs("MOTDAPI", "GetFromJE", (string addr, ushort port) =>
+        RemoteCallAPI.ExportFunc("MOTDAPI", "GetFromJE", (List<Valuetype> args) => GetFromJavaEdition(args[0], args[1], args[2]));
+        RemoteCallAPI.ExportFunc("MOTDAPI", "GetFromJE", (List<Valuetype> args) =>
         {
-            byte[] back = new byte[1024 * 8]; // 防止返回内容过多导致撑爆字节数组；包含服务器图标的返回长度一般都在5k上下
-            int length = 0;
-            try
+            Task<string> task = Task.Run(() => GetFromJavaEdition(args[0], args[1], args[4]));
+            task.ContinueWith((task) =>
             {
-                IPAddress ip = GetIP(addr);
-                Socket socket = new(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                socket.Connect(ip, port);
-                socket.Send(JEData);
-                length = socket.Receive(back);
-                socket.Close();
-            } catch (SocketException ex)
-            {
-                logger.Debug.WriteLine(ex);
-            }
-            return length > 5 ?
-                Encoding.UTF8.GetString(back, 5, length - 5) : // Java服务器返回数据包头部有5个byte无效
-                Encoding.UTF8.GetString(back);
+                if (!RemoteCallAPI.HasFunc(args[2], args[3]))
+                {
+                    return;
+                }
+                ScheduleAPI.NextTick(() =>
+                    RemoteCallAPI.ImportFunc(args[2], args[3])(new() { task.Id, task.Result }));
+            });
+            return task.Id;
         });
     }
 
-    /// <summary>
-    /// 获取IP地址
-    /// </summary>
-    /// <param name="addr">域名或IP字符串</param>
-    /// <returns>IP地址</returns>
-    private static IPAddress GetIP(string addr)
+    private static IPAddress GetIP(string address)
     {
-        if (!(IPAddress.TryParse(addr, out IPAddress ip) && ip != null)) // 无法直接转换的ip尝试使用dns解析
+        if (!IPAddress.TryParse(address, out IPAddress? ip))
         {
-            ip = Dns.GetHostAddresses(addr)[0];
+            ip = Dns.GetHostAddresses(address)[0];
         }
         return ip;
+    }
+
+    private static string GetFromBedrockEdition(string address, ushort port, int timeout = 0)
+    {
+        byte[] back = new byte[1024];
+        int length = 0;
+        try
+        {
+            IPAddress ip = GetIP(address);
+            Socket socket = new(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+            socket.Connect(ip, port);
+            socket.Send(s_bedrockEditionUnconnectedPong);
+            Task<int> task = socket.ReceiveAsync(back);
+            if (timeout > 0)
+            {
+                if (!task.Wait(timeout))
+                {
+                    return string.Empty;
+                }
+            }
+            else
+            {
+                task.Wait();
+            }
+            length = task.Result;
+        }
+        catch (SocketException)
+        {
+        }
+        return length > 35 ?
+               Encoding.UTF8.GetString(back, 35, length - 35) :
+               Encoding.UTF8.GetString(back);
+    }
+
+    private static string GetFromJavaEdition(string address, ushort port, int timeout = 0)
+    {
+        byte[] back = new byte[1024 * 8];
+        int length = 0;
+        try
+        {
+            IPAddress ip = GetIP(address);
+            Socket socket = new(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(ip, port);
+            socket.Send(s_javaEditionStatusRequest);
+            Task<int> task = socket.ReceiveAsync(back);
+            if (timeout > 0)
+            {
+                if (!task.Wait(timeout))
+                {
+                    return string.Empty;
+                }
+            }
+            else
+            {
+                task.Wait();
+            }
+            length = task.Result;
+            socket.Close();
+        }
+        catch (SocketException)
+        {
+        }
+        return length > 5 ?
+            Encoding.UTF8.GetString(back, 5, length - 5) :
+            Encoding.UTF8.GetString(back);
     }
 }
